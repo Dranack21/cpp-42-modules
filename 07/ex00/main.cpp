1b654d994670d74838417e06766b6100a674bde8
Hugo Abouda <habouda@f2r3s12.clusters.42paris.fr>
2025-05-30 18:52:30 +0200

    finished ex00

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -1,13 +1,29 @@
 #include "templates.hpp"
 
-int main()
-{
-	int a = 2;
-	int b = 5;
+// int main()
+// {
+// 	int a = 2;
+// 	int b = 5;
 
-	std::cout << "The smallest value is: "<< min(a, b) << std::endl;
-	std::cout << "The biggest value is: "<< max(a, b) << std::endl;
-	swap(a, b);
-	std::cout << "New A "<< a << std::endl;
-	std::cout << "Mew B "<< b << std::endl;
+// 	std::cout << "The smallest value is: "<< min(a, b) << std::endl;
+// 	std::cout << "The biggest value is: "<< max(a, b) << std::endl;
+// 	swap(a, b);
+// 	std::cout << "New A "<< a << std::endl;
+// 	std::cout << "Mew B "<< b << std::endl;
+// }
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
 }
