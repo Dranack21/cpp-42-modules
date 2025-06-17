ccbb889ffa29d88b6266137e79fd972842453779
root <root@Hugo>
2025-06-17 05:24:08 +0200

    finished pain 02

---
+++ 08/ex03/MutantStack.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stack>
+#include <iostream>
+
+template <typename T, typename Container = std::deque<T> >
+
+class	MutantStack : public std::stack<T, Container>
+{
+	
+	public:
+		MutantStack(): std::stack<T, Container>(){}
+		MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
+		MutantStack& operator=(const MutantStack& other)
+		{
+		if (this != &other)
+			std::stack<T, Container>::operator=(other);
+		return *this;
+		}
+		~MutantStack(){}
+
+
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
+
+		iterator	begin()
+		{
+			return (c.begin());
+		}
+		iterator	end()
+		{
+			return (c.end());
+		}
+		iterator	rbegin()
+		{
+			return (c.rbegin());
+		}
+		iterator	rend()
+		{
+			return (c.rend());
+		}
+};
