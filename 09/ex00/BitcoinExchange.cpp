5e933809af7f464ccaa4690ea002acfd618a3534
Hugo Abouda <habouda@f6r3s16.clusters.42paris.fr>
2025-06-20 22:03:36 +0200

    our house

--- 09/ex00/BitcoinExchange.cpp
+++ 09/ex00/BitcoinExchange.cpp
@@ -4,9 +4,28 @@ void	Fill_map(std::fstream &file)
 {
 	std::map<Date, double> map;
 	std::string line;
+	std::fstream file2;
 
 	while(std::getline(file, line))
 		Read_line(line ,map);
+	file2.open("input.txt");
+	Date tmp;
+	double o = 0;
+	char c = ' ';
+		std::map<Date, double>::iterator it;
+	while (std::getline(file2, line))
+	{
+		int result = sscanf(line.c_str(), "%d-%d-%d | %lf%c",&tmp.Year,&tmp.Month,&tmp.Day,&o,&c);
+		if (result == 4 && c == ' ')
+		{
+			it = map.begin();
+			while (!map[tmp])
+			{
+				
+			}
+			std::cout << "Year: " << tmp.Year << ", Month: " << tmp.Month << ", Day: " << tmp.Day << "=>" << f << std::endl;
+		}
+	}
 }
 
 void	Read_line(std::string &line, std::map<Date, double> &map)
